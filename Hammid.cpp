fc11951dd58224997d81b8bbd643556626fd1018
us313 <96589630+us313@users.noreply.github.com>
2021-12-23 22:07:04 +0500

    Delete Hammid.cpp

--- Hammid.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-cout<<"HELLO WORLD"<<endl;
-cout<<"MY NAME IS AKRAM"<<endl;
-cout<<"Major Assignments"<<endl;
-cout<<"Of "<<endl;
-cout<<"Iftekhar Alam Sir "<<endl;
-}
